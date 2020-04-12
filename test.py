09932e9e889f7d8fede4f60167aa540beb15e7d5
宮崎貴史 <>
2020-04-12 11:31:20 +0900

    small fix

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 from seven import getProdcutInfo, downloadProductImg
 from bs4 import BeautifulSoup as bs4
 
-# print(getAllListLinks())
-
 # すべての商品一覧ページのURLを取得する
 
 # 各一覧ページから商品のリンクを取得する
 
 # 各商品ページから、商品情報を取得する
+
+# 動作確認用コード
 soup = bs4(open('./files/1.html'), 'html.parser')
 # img = downloadProductImg(soup)
 info = getProdcutInfo(soup)
